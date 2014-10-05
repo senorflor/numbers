0571c37db521e7428729aea275e6a310521e7d7d
Patrick Flor <git.push.origin@gmail.com>
2014-10-04 21:31:41 -0500

    lein new

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-numbers
-=======
+# numbers
 
 Programming through _Numbers: A Very Short Introduction_, by Peter M. Higgins
+
+## Usage
+
+Can't think of one yet
+
+## License
+
+Copyright © 2014 Patrick Flor
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject numbers "0.1.0-SNAPSHOT"
+  :description "Programming through _Numbers: A Very Short Introduction_, by Peter M. Higgins"
+  :url "https://github.com/senorflor/numbers"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]])
