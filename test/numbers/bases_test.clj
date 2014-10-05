1d75ee2bcf8de9d987b3b994b17d2d23b555952c
Patrick Flor <git.push.origin@gmail.com>
2014-10-05 15:45:09 -0500

    Function composition: how does it work

--- test/numbers/bases_test.clj
+++ test/numbers/bases_test.clj
@@ -11,7 +11,7 @@
          "101010101" 5
          123456789 45))
   (testing "`sum-of-digits` works for base-n version of arbitrary notation"
-    (are [n sum] (= (sum-of-digits (second arbitrary-base-and-digits) arbitrary-base n) sum)
+    (are [n sum] (= (sum-of-digits (comp second arbitrary-base-and-digits) arbitrary-base n) sum)
          "1234:12,13,14" 39
          "123:100,0,0,100" 200
          "256:255,255,255,255" 1020)))
